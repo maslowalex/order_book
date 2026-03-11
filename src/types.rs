1ae587b9ab170d5f6ef8c7ad703e9996e2c9743c
maslowalex <maslo.rails@gmail.com>
2026-03-11 20:54:19 +0200

    Session 3

--- src/types.rs
+++ src/types.rs
@@ -91,42 +91,42 @@ impl OrderBuilder {
         }
     }
 
-    fn timestamp(mut self, timestamp: u128) -> Self {
+    pub fn timestamp(mut self, timestamp: u128) -> Self {
         self.timestamp = Some(timestamp);
         self
     }
 
-    fn exchange_id(mut self, exchange_id: impl Into<String>) -> Self {
+    pub fn exchange_id(mut self, exchange_id: impl Into<String>) -> Self {
         self.exchange_id = Some(exchange_id.into());
         self
     }
 
-    fn client_id(mut self, client_id: impl Into<String>) -> Self {
+    pub fn client_id(mut self, client_id: impl Into<String>) -> Self {
         self.client_id = Some(client_id.into());
         self
     }
 
-    fn order_type(mut self, order_type: OrderType) -> Self {
+    pub fn order_type(mut self, order_type: OrderType) -> Self {
         self.order_type = Some(order_type);
         self
     }
 
-    fn price(mut self, price: Price) -> Self {
+    pub fn price(mut self, price: Price) -> Self {
         self.price = Some(price);
         self
     }
 
-    fn side(mut self, side: Side) -> Self {
+    pub fn side(mut self, side: Side) -> Self {
         self.side = Some(side);
         self
     }
 
-    fn quantity(mut self, quantity: u64) -> Self {
+    pub fn quantity(mut self, quantity: u64) -> Self {
         self.quantity = Some(quantity);
         self
     }
 
-    fn lifecycle(mut self, lifecycle: OrderLifecycle) -> Self {
+    pub fn lifecycle(mut self, lifecycle: OrderLifecycle) -> Self {
         self.lifecycle = Some(lifecycle);
         self
     }
@@ -145,7 +145,7 @@ pub struct PriceLevel {
     pub orders: Vec<Order>,
 }
 
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 pub enum OrderError {
     InvalidSide,
 }
@@ -159,7 +159,7 @@ impl PriceLevel {
         }
     }
 
-    pub fn is_empty(self) -> bool {
+    pub fn is_empty(&self) -> bool {
         self.orders.is_empty()
     }
 
@@ -171,6 +171,21 @@ impl PriceLevel {
         self.orders.push(order);
 
         Ok(self)
+    }
+
+    pub fn remove_order(&mut self, order_exchange_id: &str) -> Option<Order> {
+        let idx = self
+            .orders
+            .iter()
+            .position(|order| order.exchange_id == order_exchange_id)?;
+
+        Some(self.orders.remove(idx))
+    }
+
+    pub fn total_quantity(&self) -> u64 {
+        self.orders
+            .iter()
+            .fold(0, |acc, order| acc + order.quantity)
     }
 }
 
@@ -225,13 +240,82 @@ mod tests {
     }
 
     #[test]
-    fn price_level_remove_order_works() {}
+    fn price_level_remove_order_works() {
+        let mut price_level = price_level_ask(None);
+        let order = Order::builder()
+            .side(Side::Ask)
+            .price(Decimal::new(100, 2))
+            .client_id("client_id")
+            .exchange_id("exchange_id")
+            .quantity(10)
+            .build();
+        let order_id = order.exchange_id.clone();
+
+        let result = price_level.add_order(order);
+
+        assert!(result.is_ok());
+
+        let result = price_level.remove_order(&order_id);
+
+        assert!(result.is_some());
+        assert!(price_level.is_empty());
+    }
 
     #[test]
-    fn price_level_remote_nonexistent_doesnt_crash() {}
+    fn price_level_remote_nonexistent_doesnt_crash() {
+        let mut price_level = price_level_ask(None);
+
+        price_level.remove_order("exchange_id");
+
+        assert!(price_level.is_empty());
+    }
 
     #[test]
-    fn price_level_is_empty_works() {}
+    fn price_level_is_empty_works() {
+        let mut price_level = price_level_ask(None);
+
+        assert!(price_level.is_empty());
+
+        let order = Order::builder()
+            .side(Side::Ask)
+            .price(Decimal::new(100, 2))
+            .client_id("client_id")
+            .exchange_id("exchange_id")
+            .quantity(10)
+            .build();
+
+        _ = price_level.add_order(order);
+
+        assert!(!price_level.is_empty());
+    }
+
+    #[test]
+    fn price_level_total_quantity_works() {
+        let mut price_level = price_level_bid(None);
+        let order1 = Order::builder()
+            .side(Side::Bid)
+            .price(Decimal::new(100, 2))
+            .client_id("client_id_1")
+            .exchange_id("order_1")
+            .quantity(10)
+            .build();
+        let order2 = Order::builder()
+            .side(Side::Bid)
+            .price(Decimal::new(100, 2))
+            .client_id("client_id_2")
+            .exchange_id("order_2")
+            .quantity(35)
+            .build();
+
+        price_level
+            .add_order(order1)
+            .expect("Order 1 doesn't added");
+        price_level
+            .add_order(order2)
+            .expect("Order 2 doesn't added");
+
+        assert_eq!(price_level.total_quantity(), 45);
+    }
 
     fn price_level_ask(price: Option<i64>) -> PriceLevel {
         let price = match price {
